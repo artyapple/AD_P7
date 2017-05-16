bf2e4b341762355b47b58ac6e3fb71303272128c
Artur Iablokov <artyapple@gmail.com>
2017-05-16 20:51:07 +0200

    Clean up

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,6 +1,5 @@
 package tree;
 import node.INode;
-import node.LinkedTreeNode;
 
 public interface BinaryTree <T extends Comparable<T>> {
 	
@@ -8,24 +7,28 @@ public interface BinaryTree <T extends Comparable<T>> {
 	 * adds node to tree 
 	 * @param node
 	 */
+	@SuppressWarnings({ "hiding", "rawtypes" })
 	public <T> void add(INode node);
 	
 	/**
 	 * prints data in order 
 	 * @param node
 	 */
+	@SuppressWarnings({ "hiding", "rawtypes" })
 	public <T> void inOrderTraverseTree(INode node);
 	
 	/**
 	 * prints data in pre order
 	 * @param node
 	 */
+	@SuppressWarnings({ "hiding", "rawtypes" })
 	public <T> void preOrderTraverseTree(INode node);
 	
 	/**
 	 * prints data in post order
 	 * @param node
 	 */
+	@SuppressWarnings({ "rawtypes", "hiding" })
 	public <T> void postOrderTraverseTree(INode node);
 
 }
